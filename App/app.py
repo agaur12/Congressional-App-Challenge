952d9b0229921c473a03e37efd963f1cd716f030
agaur12 <102323862+agaur12@users.noreply.github.com>
2023-10-17 19:08:15 +0000

    Added socket functionality for testing

---
+++ App/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, jsonify, request
+from flask_socketio import SocketIO, emit, send
+
+app = Flask(__name__)
+socket = SocketIO(app)
+
+# Store chat messages in memory (replace with a database in a production app)
+messages = []
+
+@app.route("/")
+def index():
+   return render_template('index.html')
+
+@socket.on('message')
+def handle_message(data):
+    username = 'User1'  # You can replace this with the actual username
+    message = data['message']
+    messages.append({'username': username, 'message': message})
+    emit('message', {'username': username, 'message': message}, broadcast=True)
+
+@socket.on('connect')
+def handle_connect():
+    print('User connected')
+
+@socket.on('disconnect')
+def handle_disconnect():
+    print('User disconnected')
+
+
+"""
+@app.route("/")
+def login():
+   return render_template('login.html')
+"""
+
+if __name__ == "__main__":
+    socket.run(app, debug=True)
